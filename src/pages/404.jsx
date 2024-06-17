ac80b16f4dc6a2a59ff7f2021e1ceb3963702e64
Jhody Lagarde <jhody.lagarde@outlook.fr>
2024-06-17 19:10:57 +0200

    Modification CSS

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,13 +5,13 @@ import errorImg from '../assets/404.svg'
 
 function Error() {
   return (
-    <section className="error">
+    <main className="error">
       <img src={errorImg} alt="Error 404 not found" className="error__img" />
       <p className="error__p">Oups! La page que vous demandez n'existe pas.</p>
       <Link className="error__link" to="/">
         Retourner sur la page d'accueil
       </Link>
-    </section>
+    </main>
   )
 }
 export default Error
